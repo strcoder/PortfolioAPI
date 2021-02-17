be37da537f2e970d30d56275d3c6461a91692340
Antonio <aguzmanv09@gmail.com>
2021-02-17 16:18:47 -0600

    Fix

--- build/utils/buildMessage.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var buildMessage = function (entity, action) {
-    if (action === 'list') {
-        return entity + "s " + action + "ed";
-    }
-    return entity + " " + action + "d";
-};
-exports.default = buildMessage;
--- build/utils/time.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SIXTY_MINUTES_IN_SECONDS = exports.FIVE_MINUTES_IN_SECONDS = void 0;
-exports.FIVE_MINUTES_IN_SECONDS = 300;
-exports.SIXTY_MINUTES_IN_SECONDS = 3600;
